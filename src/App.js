7fb93b5a63b181d9c6cf96170d2be1d672667d81
pushpanjali29 <pushpanjali.0029@gamil.com>
2024-11-26 12:44:48 +0530

    initial commit for react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    if (value === "C") {
+      if (inputValue === "" && result === "") {
+        setResult("Error");
+      } else {
+        setResult("");
+      }
+    } else if (value === "=" && inputValue === "") {
+      setResult("Error");
+    } else if (value === "=") {
+      if (incompleteExpression(inputValue)) {
+        setResult("Error");
+      } else {
+        try {
+          setResult(eval(inputValue));
+        } catch (e) {
+          setResult(e);
+        }
+      }
+
+      setInputValue("");
+    } else {
+      setInputValue((prev) => prev + value);
+    }
+  };
+
+  const incompleteExpression = (expression) => {
+    const lastChar = expression[expression.length - 1];
+    const operators = ["+", "-", "/", "*", "="];
+    return operators.includes(lastChar);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <input
+        type="text"
+        value={inputValue}
+        readOnly
+        // onSubmit={(e) => e.preventDefault}
+      />
+      <div className="result-div">{result}</div>
+
+      <div className="grid-container">
+        {[
+          "7",
+          "8",
+          "9",
+          "+",
+          "4",
+          "5",
+          "6",
+          "-",
+          "1",
+          "2",
+          "3",
+          "*",
+          "C",
+          "0",
+          "=",
+          "/",
+        ].map((value, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => handleButtonClick(value)}
+              className="grid-item"
+            >
+              {value}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
